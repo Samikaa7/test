1d2946764b4cea0bdb8573f935967e84901714be
Samikaa7 <139257732+Samikaa7@users.noreply.github.com>
2026-02-12 14:38:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let display = document.getElementById("display");
+
+function appendNumber(number) {
+    if (display.innerText === "0") {
+        display.innerText = number;
+    } else {
+        display.innerText += number;
+    }
+}
+
+function appendOperator(operator) {
+    let lastChar = display.innerText.slice(-1);
+    if ("+-*/".includes(lastChar)) {
+        display.innerText = display.innerText.slice(0, -1) + operator;
+    } else {
+        display.innerText += operator;
+    }
+}
+
+function clearDisplay() {
+    display.innerText = "0";
+}
+
+function calculate() {
+    try {
+        display.innerText = eval(display.innerText);
+    } catch {
+        display.innerText = "Error";
+    }
+}
